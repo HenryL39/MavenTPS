78c373c3ba480f9b00d1daca655d4fd7e1b299a9
root <root@localhost.localdomain>
2019-04-12 03:24:47 -0700

    modification du fichier d'installation d'artifactory

--- artifactory_installation.sh
+++ artifactory_installation.sh
@@ -6,18 +6,19 @@ then
     echo "Artifactory déjà existant" 
 else
     wget $ARTI_LINK
-	unzip jfrog-artifactory-pro-6.9.1.zip
-	mv artifactory-pro-6.9.1 /opt  
+    unzip download_file?file_path=org%2Fartifactory%2Fpro%2Fjfrog-artifactory-pro%2F6.9.1%2Fjfrog-artifactory-pro-6.9.1.zip
+    mv artifactory-pro-6.9.1 /opt  
 fi
 if [ !id artifactory ]
 then
 	useradd artifactory
 fi
-if [ -e "/opt/jfrog-artifactory-pro-6/bin/installService.sh" ]
+chown -R artifactory:artifactory /opt/artifactory-pro.6.9.1
+if [ -e "/etc/systemd/system/artifactory.service" ]
 then
 	echo "service artifactory déjà configuré"
 else
-	/opt/jfrog-artifactory-pro-6/bin/installService.sh
+	/opt/artifactory-pro-6.9.1/bin/installService.sh
 	systemctl daemon-reload
 	systemctl start artifactory.service
 	systemctl status artifactory.service
