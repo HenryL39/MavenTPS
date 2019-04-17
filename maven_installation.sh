0b9d86c9698971629d1c129554727630ecedaf69
HenryL39 <49313686+HenryL39@users.noreply.github.com>
2019-04-17 15:59:16 +0200

    Update maven_installation.sh

--- maven_installation.sh
+++ maven_installation.sh
@@ -24,8 +24,8 @@ else
         touch /etc/profile.d/maven.sh
         cat >/etc/profile.d/maven.sh <<EOL
         export JAVA_HOME=/usr/lib/jvm/jre-openjdk
-        export M2_HOME=/opt/apache-maven.3.6.0
-        export MAVEN_HOME=/opt/apache-maven.3.6.0
+        export M2_HOME=/opt/apache-maven-3.6.0
+        export MAVEN_HOME=/opt/apache-maven-3.6.0
         export PATH=\${M2_HOME}/bin:\${PATH}:jdk1.8.0_201/bin
 EOL
 . /etc/profile
