f66bd8bfd6bf3693f4d5aef0400465eb255f2de4
HenryL39 <49313686+HenryL39@users.noreply.github.com>
2019-04-17 05:18:34 -0700

    Update maven_installation.sh

--- maven_installation.sh
+++ maven_installation.sh
@@ -1,31 +1,32 @@
 MAVEN_LINK="http://mirrors.standaloneinstaller.com/apache/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz"
 
 cd /root/Downloads
-if [ -d "/opt/apache-maven.3.6.0" ] 
+if [ -d "/opt/apache-maven.3.6.0" ]
 then
-    echo "Maven déjà existant" 
+    echo "Maven déjà existant"
 else
     wget $MAVEN_LINK
-	tar xf apache-maven-3.6.0-bin.tar.gz
-	mv apache-maven-3.6.0 /opt
+        tar xf apache-maven-3.6.0-bin.tar.gz
+        mv apache-maven-3.6.0 /opt
 fi
-if [ -d "/opt/jdk1.8.0_201" ] 
+java -version
+if [ $? -eq 0 ]
 then
-    echo "Java déjà existant" 
-else   
-	tar xf jdk-8u201-linux-x64.tar.gz
-	mv jdk1.8.0_201 /opt
-fi
-if [ -e "/etc/profile.d/maven.sh"]
-then
-	"maven déjà configuré"
+    echo "Java déjà existant"
 else
-	touch /etc/profile.d/maven.sh
-	cat >/etc/profile.d/maven.sh <<EOL
-	export JAVA_HOME=/usr/lib/jvm/jre-openjdk
-	export M2_HOME=/opt/apache-maven.3.6.0/bin
-	export MAVEN_HOME=/opt/apache-maven.3.6.0/bin
-	export PATH=${M2_HOME}/bin:${PATH}:jdk1.8.0_201/bin
+        tar xf jdk-8u201-linux-x64.tar.gz
+        mv jdk1.8.0_201 /opt
+fi
+if [ -e "/etc/profile.d/maven.sh" ]
+then
+        "maven déjà configuré"
+else
+        touch /etc/profile.d/maven.sh
+        cat >/etc/profile.d/maven.sh <<EOL
+        export JAVA_HOME=/usr/lib/jvm/jre-openjdk
+        export M2_HOME=/opt/apache-maven.3.6.0
+        export MAVEN_HOME=/opt/apache-maven.3.6.0
+        export PATH=\${M2_HOME}/bin:\${PATH}:jdk1.8.0_201/bin
 EOL
 . /etc/profile
 fi
@@ -33,5 +34,5 @@ fi
 mvn -v
 if [ $? -eq 0 ]
 then
-	echo "installation terminée"
+        echo "installation terminée"
 fi
